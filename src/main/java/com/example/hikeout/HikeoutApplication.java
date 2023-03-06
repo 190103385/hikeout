5719977ce6ed4d68ca09c4b0f4ff263a56c7d22f
Nurdaulet Kurmangazyuly <knurdaulet0109@gmail.com>
2023-03-06 10:48:11 +0600

    HO-0: Init project

---
+++ src/main/java/com/example/hikeout/HikeoutApplication.java
@@ -0,0 +1,17 @@
+package com.example.hikeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class HikeoutApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        SpringApplication.run(HikeoutApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/hikeout/configs/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.example.hikeout.configs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI().info(new Info().title("Spring Doc").version("1.0.0"));
+    }
+}
