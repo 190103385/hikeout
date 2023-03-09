810ebbcfcdbd0ddbff8e9e2055ec7967bb427c23
Nurdaulet Kurmangazyuly <knurdaulet0109@gmail.com>
2023-03-09 16:27:08 +0600

    BACK-1: Implement getAllLocations request

--- src/main/java/com/example/hikeout/controllers/LocationsController.java
+++ src/main/java/com/example/hikeout/controllers/LocationsController.java
@@ -1,8 +1,9 @@
 package com.example.hikeout.controllers;
 
-import com.example.hikeout.domains.Location;
-import com.example.hikeout.repositories.LocationRepository;
+import com.example.hikeout.dto.LocationDto;
+import com.example.hikeout.services.ILocationsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/location")
+@RequestMapping("api/locations")
 public class LocationsController {
 
     @Autowired
-    private LocationRepository locationRepository;
+    private ILocationsService service;
 
     @GetMapping
-    public List<Location> getAllLocations() {
-        return locationRepository.findAll();
+    public List<LocationDto> getAllLocations() {
+        return service.getAllLocations();
     }
 
 }
--- src/main/java/com/example/hikeout/services/impl/LocationsServiceImpl.java
+++ src/main/java/com/example/hikeout/services/impl/LocationsServiceImpl.java
@@ -2,9 +2,10 @@ package com.example.hikeout.services.impl;
 
 import com.example.hikeout.domains.Location;
 import com.example.hikeout.dto.LocationDto;
-import com.example.hikeout.mappers.LocationToLocationDto;
+import com.example.hikeout.mappers.LocationToDto;
 import com.example.hikeout.repositories.LocationRepository;
 import com.example.hikeout.services.ILocationsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,8 +13,10 @@ import java.util.List;
 @Service
 public class LocationsServiceImpl implements ILocationsService {
 
+    @Autowired
     LocationRepository locationRepository;
-    LocationToLocationDto mapper;
+    @Autowired
+    LocationToDto mapper;
 
     @Override
     public List<LocationDto> getAllLocations() {
